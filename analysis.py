ead456ca1782d2624ac7b6772b011a37f8b05314
Stella Kim <skim0922@uw.edu>
2020-08-15 16:26:46 -0700

    pull trial

--- analysis.py
+++ analysis.py
@@ -5,7 +5,8 @@ File to perform data analysis
 def function():
     print('hello world')
     print('I think I figured it out!')
-    
+    print('pull test')
+
 def main():
 
 if __name__ == '__main__':
