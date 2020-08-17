bf10be9b002643846db7b0436099edfd17857fa8
Sahana Sundar <sahana@Sahanas-MacBook-Pro.local>
2020-08-16 17:42:30 -0700

    created data frames

--- clean_data.py
+++ clean_data.py
@@ -2,7 +2,19 @@
 File to clean and sort data
 '''
 
+import pandas as pd
+
+
 def main():
+    # creating master dataframe with everything in it 
+    df = pd.read_csv('/Users/sahana/Desktop/github/cse163-final-project/'
+                     'data/Racial_and_Social_Equity_Composite_Index.csv')
+    # filtered data
+    data = df[['PCT_ADULTMENTALHEALTHNOTGOOD', 'RACE_ELL_ORIGINS_PERCENTILE',
+               'SOCIOECONOMIC_PERCENTILE', 'PCT_ENGLISH_LESSTHAN_VERY_WELL',
+               'PCT_LESS_BACHELOR_DEGREE', 'ACRES_TOTAL', 'COMPOSITE_QUINTILE',
+              'NAME10']]
+ 
 
 if __name__ == '__main__':
     main()
