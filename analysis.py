feb2f4e60b5b7db8521b543ef06652c80076ef9f
Sahana Sundar <sahana@Sahanas-MacBook-Pro.local>
2020-08-17 16:28:44 -0700

    started plots

--- analysis.py
+++ analysis.py
@@ -2,12 +2,38 @@
 File to perform data analysis
 '''
 
-def function():
-    print('hello world')
-    print('I think I figured it out!')
-    print('pull test')
+# import clean_data --> need to figure out how to access the dataframe from
+# the other file
+import pandas as pd
+import seaborn as sns 
+import matplotlib.pyplot as plt
+
+
+def plot_problem_1():
+    df = pd.read_csv('/Users/sahana/Desktop/github/cse163-final-project/'
+                     'data/Racial_and_Social_Equity_Composite_Index.csv') 
+    df = df.dropna()
+    data = df[['PCT_ADULTMENTALHEALTHNOTGOOD', 'RACE_ELL_ORIGINS_PERCENTILE',
+               'SOCIOECONOMIC_PERCENTILE', 'PCT_ENGLISH_LESSTHAN_VERY_WELL',
+               'PCT_LESS_BACHELOR_DEGREE', 'ACRES_TOTAL', 'COMPOSITE_QUINTILE',
+               'NAME10']]    
+    # race vs.poor mental health  
+    sns.relplot(x = 'RACE_ELL_ORIGINS_PERCENTILE',
+                y = 'PCT_ADULTMENTALHEALTHNOTGOOD',
+                kind = 'scatter', data = data) 
+    plt.savefig('racevsmentalhealth.png')
+
+    # socioeconomic vs poor mental health 
+    sns.relplot(x = 'SOCIOECONOMIC_PERCENTILE',
+                y = 'PCT_ADULTMENTALHEALTHNOTGOOD',
+                kind = 'scatter', data = data) 
+    plt.savefig('racevsmentalhealth.png') 
+
+
 
 def main():
+    plot_problem_1()
+
 
 if __name__ == '__main__':
     main()
--- clean_data.py
+++ clean_data.py
@@ -9,12 +9,12 @@ def main():
     # creating master dataframe with everything in it 
     df = pd.read_csv('/Users/sahana/Desktop/github/cse163-final-project/'
                      'data/Racial_and_Social_Equity_Composite_Index.csv')
-    # filtered data
+
     data = df[['PCT_ADULTMENTALHEALTHNOTGOOD', 'RACE_ELL_ORIGINS_PERCENTILE',
                'SOCIOECONOMIC_PERCENTILE', 'PCT_ENGLISH_LESSTHAN_VERY_WELL',
                'PCT_LESS_BACHELOR_DEGREE', 'ACRES_TOTAL', 'COMPOSITE_QUINTILE',
-              'NAME10']]
- 
+              'NAME10']] 
+
 
 if __name__ == '__main__':
     main()
