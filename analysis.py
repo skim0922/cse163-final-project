2eeb2525df2d9f0981129cc0ceb306043b96fdff
Stella Kim <skim0922@uw.edu>
2020-08-18 16:22:09 -0700

    fixed import and flake8

--- analysis.py
+++ analysis.py
@@ -2,36 +2,31 @@
 File to perform data analysis
 '''
 
-# import clean_data --> need to figure out how to access the dataframe from
-# the other file
-# from clean_data import filter_data: i tried this like they do in the hw but it didn't work
-import pandas as pd
-import seaborn as sns 
+from clean_data import filter_data
+import seaborn as sns
 import matplotlib.pyplot as plt
 
 
-def plot_problem_1(data):
-    # race vs.poor mental health  
-    sns.relplot(x = 'RACE_ELL_ORIGINS_PERCENTILE',
-                y = 'PCT_ADULTMENTALHEALTHNOTGOOD',
-                kind = 'scatter', data = data) 
+def plot_problem_1(clean_data):
+    # race vs.poor mental health
+    sns.relplot(x='RACE_ELL_ORIGINS_PERCENTILE',
+                y='PCT_ADULTMENTALHEALTHNOTGOOD',
+                kind='scatter', data=clean_data)
     plt.savefig('race_vs_mentalhealth.png')
 
-    # socioeconomic vs poor mental health 
-    sns.relplot(x = 'SOCIOECONOMIC_PERCENTILE',
-                y = 'PCT_ADULTMENTALHEALTHNOTGOOD',
-                kind = 'scatter', data = data) 
-    plt.savefig('socioecon_vs_mentalhealth.png') 
-    #plots not saving need to figure that out :) 
-    # plots saved now!
+    # socioeconomic vs poor mental health
+    sns.relplot(x='SOCIOECONOMIC_PERCENTILE',
+                y='PCT_ADULTMENTALHEALTHNOTGOOD',
+                kind='scatter', data=clean_data)
+    plt.savefig('socioecon_vs_mentalhealth.png')
 
 
-def plot_question_three(data):
+def plot_question_three(clean_data):
     # total acres vs. index quintile
-    #grouped = data.groupby('COMPOSITE_QUINTILE')['ACRES_TOTAL'].mean()
-    sns.catplot(x = 'COMPOSITE_QUINTILE',
-                y = 'ACRES_TOTAL',
-                data=data,
+    # grouped = data.groupby('COMPOSITE_QUINTILE')['ACRES_TOTAL'].mean()
+    sns.catplot(x='COMPOSITE_QUINTILE',
+                y='ACRES_TOTAL',
+                data=clean_data,
                 kind='bar')
     plt.xlabel('Composite Quintile')
     plt.ylabel('Acres Total')
@@ -40,18 +35,9 @@ def plot_question_three(data):
 
 
 def main():
-    # Sahana's path: df = pd.read_csv('/Users/sahana/Desktop/github/cse163-final-project/'
-    #                 'data/Racial_and_Social_Equity_Composite_Index.csv') 
-    # Stella's path:
-    df = pd.read_csv('/Users/stella/Desktop/cse163-final-project/data/Racial_and_Social_Equity_Composite_Index.csv')
-
-    df = df.dropna()
-    data = df[['PCT_ADULTMENTALHEALTHNOTGOOD', 'RACE_ELL_ORIGINS_PERCENTILE',
-               'SOCIOECONOMIC_PERCENTILE', 'PCT_ENGLISH_LESSTHAN_VERY_WELL',
-               'PCT_LESS_BACHELOR_DEGREE', 'ACRES_TOTAL', 'COMPOSITE_QUINTILE',
-               'NAME10']]   
-    plot_problem_1(data)
-    plot_question_three(data)
+    clean_data = filter_data()
+    plot_problem_1(clean_data)
+    plot_question_three(clean_data)
 
 
 if __name__ == '__main__':
--- clean_data.py
+++ clean_data.py
@@ -4,19 +4,21 @@ File to clean and sort data
 
 import pandas as pd
 
+
 def filter_data():
-    # creating master dataframe with everything in it 
-    # Sahana's path: df = pd.read_csv('/Users/sahana/Desktop/github/cse163-final-project/'
-    #                 'data/Racial_and_Social_Equity_Composite_Index.csv')
-    # Stella's path: 
+    # creating master dataframe with everything in it
+    # Sahana's path: df = pd.read_csv('/Users/sahana/Desktop/github/
+    #                                 'cse163-final-project/data/Racial_and_Social_Equity_Composite_Index.csv')
+    # Stella's path:
     df = pd.read_csv('/Users/stella/Desktop/cse163-final-project/'
                      'data/Racial_and_Social_Equity_Composite_Index.csv')
     df = df.dropna()
-
     data = df[['PCT_ADULTMENTALHEALTHNOTGOOD', 'RACE_ELL_ORIGINS_PERCENTILE',
                'SOCIOECONOMIC_PERCENTILE', 'PCT_ENGLISH_LESSTHAN_VERY_WELL',
                'PCT_LESS_BACHELOR_DEGREE', 'ACRES_TOTAL', 'COMPOSITE_QUINTILE',
-              'NAME10']] 
+              'NAME10']]
+    return data
+
 
 def main():
     filter_data()
