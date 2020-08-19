12ed2d43c1919d9429f87533c2f46bfc162f58b3
Stella Kim <skim0922@uw.edu>
2020-08-18 17:41:03 -0700

    made horizontal

--- analysis.py
+++ analysis.py
@@ -7,81 +7,49 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 
-<<<<<<< Updated upstream
-def plot_problem_1(clean_data):
+def plot_problem_one(clean_data):
     # race vs.poor mental health
     sns.relplot(x='RACE_ELL_ORIGINS_PERCENTILE',
                 y='PCT_ADULTMENTALHEALTHNOTGOOD',
                 kind='scatter', data=clean_data)
-    plt.savefig('race_vs_mentalhealth.png')
+    plt.xlabel('Race, ELL, and Origins Index Percentile')
+    plt.ylabel('Percentage of Adults Without Good Mental Health')
+    plt.title('Race, English Language Learners(ELL), and Origins Index'
+              'Percentile vs Percentage of Adults Without Good Mental Health')
+    plt.savefig('race_vs_mentalhealth.png', bbox_inches='tight')
 
+
+def plot_problem_two(clean_data):
     # socioeconomic vs poor mental health
     sns.relplot(x='SOCIOECONOMIC_PERCENTILE',
                 y='PCT_ADULTMENTALHEALTHNOTGOOD',
                 kind='scatter', data=clean_data)
-    plt.savefig('socioecon_vs_mentalhealth.png')
-
-=======
-def plot_problem_one(data):
-    # race vs.poor mental health
-    sns.relplot(x = 'RACE_ELL_ORIGINS_PERCENTILE',
-                y = 'PCT_ADULTMENTALHEALTHNOTGOOD',
-                kind = 'scatter', data=data) 
-    plt.xlabel('Race, ELL, and Origins Index Percentile')
-    plt.ylabel('Percentage of Adults Without Good Mental Health') 
-    plt.tile('Race, English Language Learners(ELL), and Origins Index'
-             'Percentile vs Percentage of Adults Without Good Mental Health')
-    plt.savefig('race_vs_mentalhealth.png', bbox_inches='tight')
-
-
-def plot_problem_two(data): 
-    # socioeconomic vs poor mental health
-    sns.relplot(x = 'SOCIOECONOMIC_PERCENTILE',
-                y = 'PCT_ADULTMENTALHEALTHNOTGOOD',
-                kind = 'scatter', data = data) 
     plt.xlabel('Socioeconomic Disadvantage Index Percentile')
-    plt.ylabel('Percentage of Adults Without Good Mental Health') 
-    plt.tile('Socioeconomic Disadvantage Index'
-             'Percentile vs Percentage of Adults Without Good Mental Health')
-    plt.savefig('socioecon_vs_mentalhealth.png', bbox_inches='tight') 
-    
->>>>>>> Stashed changes
+    plt.ylabel('Percentage of Adults Without Good Mental Health')
+    plt.title('Socioeconomic Disadvantage Index'
+              'Percentile vs Percentage of Adults Without Good Mental Health')
+    plt.savefig('socioecon_vs_mentalhealth.png', bbox_inches='tight')
+
 
 def plot_question_three(clean_data):
     # total acres vs. index quintile
     # grouped = data.groupby('COMPOSITE_QUINTILE')['ACRES_TOTAL'].mean()
-    sns.catplot(x='COMPOSITE_QUINTILE',
-                y='ACRES_TOTAL',
+    sns.catplot(x='ACRES_TOTAL',
+                y='COMPOSITE_QUINTILE',
                 data=clean_data,
                 kind='bar')
-    plt.xlabel('Composite Quintile')
-    plt.ylabel('Acres Total')
+
+    plt.xlabel('Acres Total')
+    plt.ylabel('Composite Index Quintile')
     plt.title('Total Neighborhood Acres by Composite Index Quintile')
-    plt.savefig('size_vs_quintile.png')
+    plt.savefig('size_vs_quintile.png', bbox_inches='tight')
 
 
 def main():
-<<<<<<< Updated upstream
     clean_data = filter_data()
-    plot_problem_1(clean_data)
+    plot_problem_one(clean_data)
+    plot_problem_two(clean_data)
     plot_question_three(clean_data)
-=======
-    # Sahana's path:
-     df = pd.read_csv('/Users/sahana/Desktop/github/cse163-final-project/'
-                   'data/Racial_and_Social_Equity_Composite_Index.csv') 
-    # Stella's path:
-    # df = pd.read_csv('/Users/stella/Desktop/cse163-final-project/data/
-    # Racial_and_Social_Equity_Composite_Index.csv')
-
-    df = df.dropna()
-    data = df[['PCT_ADULTMENTALHEALTHNOTGOOD', 'RACE_ELL_ORIGINS_PERCENTILE',
-               'SOCIOECONOMIC_PERCENTILE', 'PCT_ENGLISH_LESSTHAN_VERY_WELL',
-               'PCT_LESS_BACHELOR_DEGREE', 'ACRES_TOTAL', 'COMPOSITE_QUINTILE',
-               'NAME10']]   
-    plot_problem_one(data)
-    plot_problem_two(data)
-    plot_question_three(data)
->>>>>>> Stashed changes
 
 
 if __name__ == '__main__':
